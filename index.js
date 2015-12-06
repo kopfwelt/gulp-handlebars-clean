8e450920197f4949bc7ebe72bf5c99078c7eb438
Steve Sinnwell <steve@kopfwelt.com>
2015-12-06 21:49:54 +0100

    [update] implemented cleanup

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 var through = require('through2');
 
 module.exports = function handlebarsStripComments(options) {
-  options = options || {};
 
   function stripComments(file, encoding, callback) {
-    var options = setup(opts || {});
+    var options = options || {};
 
     if (file.isNull()) {
       return callback(null, file);
@@ -15,34 +14,13 @@ module.exports = function handlebarsStripComments(options) {
     if (file.isStream()) {
       return callback(createError(file, 'Streaming not supported'));
     }
-/*
-    if (file.sourceMap) {
-      options.outSourceMap = file.relative;
-    }
 
     var originalContents = String(file.contents);
-
-    var mangled = trycatch(function () {
-      var m = uglify.minify(String(file.contents), options);
-      m.code = new Buffer(m.code.replace(reSourceMapComment, ''));
-      return m;
-    }, createError.bind(null, file));
-
-    if (mangled instanceof PluginError) {
-      return callback(mangled);
-    }
-
-    file.contents = mangled.code;
-
-    if (file.sourceMap) {
-      var sourceMap = JSON.parse(mangled.map);
-      sourceMap.sources = [file.relative];
-      sourceMap.sourcesContent = [originalContents];
-      applySourceMap(file, sourceMap);
-    }
-*/
-
-    console.log(file.contents);
+    var strippedContents = new Buffer(originalContents
+      .replace(/\{\{!--[^]*?--\}\}/gm, '')
+      .replace(/^\s*[\r\n]/gm, '')
+    );
+    file.contents = strippedContents;
     
     callback(null, file);
   }
